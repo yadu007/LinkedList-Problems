1fbf9da23e395718fe72df7a92598d42cf573201
Yadu <yadu@localhost.localdomain>
2017-07-01 10:50:24 -0500

    grtnth

---
+++ GetNth.c
@@ -0,0 +1,92 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+
+typedef struct NODE_MAIN{
+int data;
+struct NODE_MAIN* next;
+}Node;
+
+void Add(Node** headRef, int newData);
+int Count(Node* head, int searchFor);
+Node* BuildOneTwoThree();
+Node* head = NULL; 
+
+int main(){
+  Node* List = BuildOneTwoThree();
+  printf("List: ");
+  Node *current;
+  printf("[ ");
+
+  for(current =head;current != NULL; current = current->next)
+    {   
+     printf("%d%s",current->data," ");
+    }
+
+  printf("]");
+  printf("\n");
+  int count = Count(List,2);
+  printf("%d is in %d nd position",count,2);
+  }
+
+Node* BuildOneTwoThree(){
+  int i;
+  for(i=1;i<4;i++)
+    {
+       Add(&head,i);
+    }
+  return(head);
+}
+
+void Add(Node** headRef, int newData){
+  Node *newNode;
+  Node *current= *headRef;
+  newNode = (Node*)malloc(sizeof(Node));
+
+  if(newNode == NULL)
+    { 
+      printf("cant allocate\n");
+      exit(0);
+    }
+
+  newNode->data = newData;
+  newNode->next = NULL;
+
+  if(head == NULL)
+    { 
+      head = newNode;
+    }
+  else
+    {
+      while(current->next != NULL)
+     {
+  current = current->next;
+     }
+     current->next = newNode;
+    // current->next->data=newData;
+     //current->next->next=NULL;
+      }
+}
+
+void displayll(Node* head){
+
+//current=head;
+//while (current!= NULL) {
+//current = current->next;
+//printf("%d",current->data);}
+}
+
+int Count(Node* head,int num){
+  Node* current;
+  int count=0;
+  int k=0;
+  current=head;
+while (current != NULL)
+    {
+       if (count == num)
+          return(current->data);
+       count++;
+       current = current->next;
+    }
+    assert(0);
+    }
